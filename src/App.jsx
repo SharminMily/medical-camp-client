5ccc9e5034a9b30fbe116ba22baf8dd52b9078dd
SharminMily <sharminmily039@gmail.com>
2023-11-25 12:15:12 +0600

    react router setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+ 
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <br />
+      <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 
+      hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Cyan</button>
+
+      
+    </>
+  )
+}
+
+export default App
